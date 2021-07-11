davidkydd/aks-periscope

commit cce800065d8835cb1588a48a0176967e45d54cb2

Code improvement and refactor (#65)
Removes all uses of files so there is no need to mount a volume on the container.

cmd/aks-periscope/aks-periscope.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"log"
 	"os"
 	"strings"
@@ -14,15 +15,22 @@ import (
 )
 
 func main() {
-	zipAndExportMode := true
-	exporter := &exporter.AzureBlobExporter{}
-
-	err := utils.CreateCRD()
+	creationTimeStamp, err := utils.GetCreationTimeStamp()
 	if err != nil {
+		log.Fatalf("Failed to get creation timestamp: %v", err)
+	}
+
+	hostname, err := utils.GetHostName()
+	if err != nil {
+		log.Fatalf("Failed to get the hostname on which AKS Periscope is running: %v", err)
+	}
+
+	if err := utils.CreateCRD(); err != nil {
 		log.Fatalf("Failed to create CRD: %v", err)
 	}
 
 	collectorList := strings.Fields(os.Getenv("COLLECTOR_LIST"))
+	exp := exporter.NewAzureBlobExporter(creationTimeStamp, hostname)
 
 	// Copies self-signed cert information to container if application is running on Azure Stack Cloud.
 	// We need the cert in order to communicate with the storage account.
@@ -32,23 +40,26 @@ func main() {
 		}
 	}
 
-	collectors := []interfaces.Collector{}
-	containerLogsCollector := collector.NewContainerLogsCollector(exporter)
-	networkOutboundCollector := collector.NewNetworkOutboundCollector(5, exporter)
-	dnsCollector := collector.NewDNSCollector(exporter)
-	kubeObjectsCollector := collector.NewKubeObjectsCollector(exporter)
-	systemLogsCollector := collector.NewSystemLogsCollector(exporter)
-	ipTablesCollector := collector.NewIPTablesCollector(exporter)
-	nodeLogsCollector := collector.NewNodeLogsCollector(exporter)
-	kubeletCmdCollector := collector.NewKubeletCmdCollector(exporter)
-	systemPerfCollector := collector.NewSystemPerfCollector(exporter)
-	helmCollector := collector.NewHelmCollector(exporter)
-	osmCollector := collector.NewOsmCollector(exporter)
+	dataProducers := []interfaces.DataProducer{}
 
-	collectors = append(collectors, containerLogsCollector)
-	collectors = append(collectors, dnsCollector)
-	collectors = append(collectors, kubeObjectsCollector)
-	collectors = append(collectors, networkOutboundCollector)
+	containerLogsCollector := collector.NewContainerLogsCollector()
+	networkOutboundCollector := collector.NewNetworkOutboundCollector()
+	dnsCollector := collector.NewDNSCollector()
+	kubeObjectsCollector := collector.NewKubeObjectsCollector()
+	systemLogsCollector := collector.NewSystemLogsCollector()
+	ipTablesCollector := collector.NewIPTablesCollector()
+	nodeLogsCollector := collector.NewNodeLogsCollector()
+	kubeletCmdCollector := collector.NewKubeletCmdCollector()
+	systemPerfCollector := collector.NewSystemPerfCollector()
+	helmCollector := collector.NewHelmCollector()
+	osmCollector := collector.NewOsmCollector()
+
+	collectors := []interfaces.Collector{
+		containerLogsCollector,
+		dnsCollector,
+		kubeObjectsCollector,
+		networkOutboundCollector,
+	}
 
 	if contains(collectorList, "connectedCluster") {
 		collectors = append(collectors, helmCollector)
@@ -67,6 +78,7 @@ func main() {
 	collectorGrp := new(sync.WaitGroup)
 
 	for _, c := range collectors {
+		dataProducers = append(dataProducers, c)
 		collectorGrp.Add(1)
 		go func(c interfaces.Collector) {
 			defer collectorGrp.Done()
@@ -79,8 +91,7 @@ func main() {
 			}
 
 			log.Printf("Collector: %s, export data", c.GetName())
-			err = c.Export()
-			if err != nil {
+			if err = exp.Export(c); err != nil {
 				log.Printf("Collector: %s, export data failed: %v", c.GetName(), err)
 			}
 		}(c)
@@ -88,13 +99,15 @@ func main() {
 
 	collectorGrp.Wait()
 
-	diagnosers := []interfaces.Diagnoser{}
-	diagnosers = append(diagnosers, diagnoser.NewNetworkConfigDiagnoser(dnsCollector, kubeletCmdCollector, exporter))
-	diagnosers = append(diagnosers, diagnoser.NewNetworkOutboundDiagnoser(networkOutboundCollector, exporter))
+	diagnosers := []interfaces.Diagnoser{
+		diagnoser.NewNetworkConfigDiagnoser(dnsCollector, kubeletCmdCollector),
+		diagnoser.NewNetworkOutboundDiagnoser(networkOutboundCollector),
+	}
 
 	diagnoserGrp := new(sync.WaitGroup)
 
 	for _, d := range diagnosers {
+		dataProducers = append(dataProducers, d)
 		diagnoserGrp.Add(1)
 		go func(d interfaces.Diagnoser) {
 			defer diagnoserGrp.Done()
@@ -107,8 +120,7 @@ func main() {
 			}
 
 			log.Printf("Diagnoser: %s, export data", d.GetName())
-			err = d.Export()
-			if err != nil {
+			if err = exp.Export(d); err != nil {
 				log.Printf("Diagnoser: %s, export data failed: %v", d.GetName(), err)
 			}
 		}(d)
@@ -116,39 +128,13 @@ func main() {
 
 	diagnoserGrp.Wait()
 
-	if zipAndExportMode {
-		log.Print("Zip and export result files")
-		err := zipAndExport(exporter)
-		if err != nil {
-			log.Fatalf("Failed to zip and export result files: %v", err)
+	zip, err := exporter.Zip(dataProducers)
+	if err != nil {
+		log.Printf("Could not zip data: %v", err)
+	} else {
+		if err := exp.ExportReader(hostname+".zip", bytes.NewReader(zip.Bytes())); err != nil {
+			log.Printf("Could not export zip archive: %v", err)
 		}
-	}
-}
-
-// zipAndExport zip the results and export
-func zipAndExport(exporter interfaces.Exporter) error {
-	hostName, err := utils.GetHostName()
-	if err != nil {
-		return err
-	}
-
-	creationTimeStamp, err := utils.GetCreationTimeStamp()
-	if err != nil {
-		return err
-	}
-
-	sourcePathOnHost := "/var/log/aks-periscope/" + strings.Replace(creationTimeStamp, ":", "-", -1) + "/" + hostName
-	zipFileOnHost := sourcePathOnHost + "/" + hostName + ".zip"
-	zipFileOnContainer := strings.TrimPrefix(zipFileOnHost, "/var/log")
-
-	_, err = utils.RunCommandOnHost("zip", "-r", zipFileOnHost, sourcePathOnHost)
-	if err != nil {
-		return err
-	}
-
-	err = exporter.Export([]string{zipFileOnContainer})
-	if err != nil {
-		return err
 	}
 
 	// TODO: Hack: for now AKS-Periscope is running as a deamonset so it shall not stop (or the pod will be restarted)
@@ -157,7 +143,6 @@ func zipAndExport(exporter interfaces.Exporter) error {
 
 	// TODO: remove this //nolint comment once the select{} has been removed
 	//nolint:govet
-	return nil
 }
 
 func contains(flagsList []string, flag string) bool {

pkg/diagnoser/networkconfig_diagnoser.go
@@ -1,17 +1,12 @@
 package diagnoser
 
 import (
-	"bufio"
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
-	"os"
-	"path/filepath"
 	"strconv"
 	"strings"
 
 	"github.com/Azure/aks-periscope/pkg/collector"
-	"github.com/Azure/aks-periscope/pkg/interfaces"
 	"github.com/Azure/aks-periscope/pkg/utils"
 )
 
@@ -25,23 +20,22 @@ type networkConfigDiagnosticDatum struct {
 
 // NetworkConfigDiagnoser defines a NetworkConfig Diagnoser struct
 type NetworkConfigDiagnoser struct {
-	BaseDiagnoser
 	dnsCollector        *collector.DNSCollector
 	kubeletCmdCollector *collector.KubeletCmdCollector
+	data                map[string]string
 }
 
-var _ interfaces.Diagnoser = &NetworkConfigDiagnoser{}
-
 // NewNetworkConfigDiagnoser is a constructor
-func NewNetworkConfigDiagnoser(dnsCollector *collector.DNSCollector, kubeletCmdCollector *collector.KubeletCmdCollector, exporter interfaces.Exporter) *NetworkConfigDiagnoser {
+func NewNetworkConfigDiagnoser(dnsCollector *collector.DNSCollector, kubeletCmdCollector *collector.KubeletCmdCollector) *NetworkConfigDiagnoser {
 	return &NetworkConfigDiagnoser{
-		BaseDiagnoser: BaseDiagnoser{
-			diagnoserType: NetworkConfig,
-			exporter:      exporter,
-		},
 		dnsCollector:        dnsCollector,
 		kubeletCmdCollector: kubeletCmdCollector,
+		data:                make(map[string]string),
 	}
+}
+
+func (collector *NetworkConfigDiagnoser) GetName() string {
+	return "networkconfig"
 }
 
 // Diagnose implements the interface method
@@ -50,54 +44,28 @@ func (diagnoser *NetworkConfigDiagnoser) Diagnose() error {
 	if err != nil {
 		return err
 	}
-	rootPath, err := utils.CreateDiagnosticDir()
-	if err != nil {
-		return err
-	}
-
-	networkDiagnosticFile := filepath.Join(rootPath, diagnoser.GetName())
-
-	f, err := os.OpenFile(networkDiagnosticFile, os.O_CREATE|os.O_WRONLY, 0644)
-	if err != nil {
-		return fmt.Errorf("Fail to open file %s: %+v", networkDiagnosticFile, err)
-	}
-	defer f.Close()
 
 	networkConfigDiagnosticData := networkConfigDiagnosticDatum{HostName: hostName}
-	for _, file := range diagnoser.dnsCollector.GetCollectorFiles() {
-		t, err := os.Open(file)
-		if err != nil {
-			return fmt.Errorf("Fail to open file %s: %+v", file, err)
-		}
-		defer t.Close()
-
-		dnsLevel := filepath.Base(file)
+	for key, data := range diagnoser.dnsCollector.GetData() {
 		var dns []string
-		scanner := bufio.NewScanner(t)
-		for scanner.Scan() {
-			s := strings.Split(scanner.Text(), " ")
-			if s[0] == "nameserver" {
-				dns = append(dns, strings.TrimSuffix(s[1], "\n"))
+		words := strings.Split(data, " ")
+		for i := range words {
+			if words[i] == "nameserver" {
+				dns = append(dns, strings.TrimSuffix(words[i+1], "\n"))
 			}
 		}
 
-		if dnsLevel == "virtualmachine" {
+		if key == "virtualmachine" {
 			networkConfigDiagnosticData.VirtualMachineDNS = dns
 		}
 
-		if dnsLevel == "kubernetes" {
+		if key == "kubernetes" {
 			networkConfigDiagnosticData.KubernetesDNS = dns
 		}
 	}
 
-	for _, file := range diagnoser.kubeletCmdCollector.GetCollectorFiles() {
-		f, err := ioutil.ReadFile(file)
-		if err != nil {
-			return fmt.Errorf("Fail to read file %s: %+v", file, err)
-		}
-
-		parts := strings.Split(string(f), " ")
-
+	for _, data := range diagnoser.kubeletCmdCollector.GetData() {
+		parts := strings.Split(data, " ")
 		for _, part := range parts {
 			if strings.HasPrefix(part, "--network-plugin=") {
 				networkPlugin := part[17:]
@@ -118,20 +86,18 @@ func (diagnoser *NetworkConfigDiagnoser) Diagnose() error {
 
 	dataBytes, err := json.Marshal(networkConfigDiagnosticData)
 	if err != nil {
-		return fmt.Errorf("Fail to marshal data: %+v", err)
+		return fmt.Errorf("marshal data from NetworkConfig Diagnoser: %w", err)
 	}
 
-	_, err = f.WriteString(string(dataBytes))
-	if err != nil {
-		return fmt.Errorf("Fail to write data to file: %+v", err)
-	}
+	diagnoser.data["networkconfig"] = string(dataBytes)
 
-	diagnoser.AddToDiagnoserFiles(networkDiagnosticFile)
-
-	err = utils.WriteToCRD(networkDiagnosticFile, diagnoser.GetName())
-	if err != nil {
-		return fmt.Errorf("Fail to write file %s to CRD: %+v", networkDiagnosticFile, err)
+	if err = utils.WriteToCRD(string(dataBytes), diagnoser.GetName()); err != nil {
+		return fmt.Errorf("write data from NetworkConfig Diagnoser to CRD: %w", err)
 	}
 
 	return nil
+}
+
+func (collector *NetworkConfigDiagnoser) GetData() map[string]string {
+	return collector.data
 }

pkg/diagnoser/networkoutbound_diagnoser.go
@@ -1,16 +1,13 @@
 package diagnoser
 
 import (
-	"bufio"
 	"encoding/json"
 	"fmt"
 	"log"
-	"os"
-	"path/filepath"
+	"strings"
 	"time"
 
 	"github.com/Azure/aks-periscope/pkg/collector"
-	"github.com/Azure/aks-periscope/pkg/interfaces"
 	"github.com/Azure/aks-periscope/pkg/utils"
 )
 
@@ -24,21 +21,20 @@ type networkOutboundDiagnosticDatum struct {
 
 // NetworkOutboundDiagnoser defines a NetworkOutbound Diagnoser struct
 type NetworkOutboundDiagnoser struct {
-	BaseDiagnoser
 	networkOutboundCollector *collector.NetworkOutboundCollector
+	data                     map[string]string
 }
 
-var _ interfaces.Diagnoser = &NetworkOutboundDiagnoser{}
-
 // NewNetworkOutboundDiagnoser is a constructor
-func NewNetworkOutboundDiagnoser(networkOutboundCollector *collector.NetworkOutboundCollector, exporter interfaces.Exporter) *NetworkOutboundDiagnoser {
+func NewNetworkOutboundDiagnoser(networkOutboundCollector *collector.NetworkOutboundCollector) *NetworkOutboundDiagnoser {
 	return &NetworkOutboundDiagnoser{
-		BaseDiagnoser: BaseDiagnoser{
-			diagnoserType: NetworkOutbound,
-			exporter:      exporter,
-		},
 		networkOutboundCollector: networkOutboundCollector,
+		data:                     make(map[string]string),
 	}
+}
+
+func (collector *NetworkOutboundDiagnoser) GetName() string {
+	return "networkoutbound"
 }
 
 // Diagnose implements the interface method
@@ -47,35 +43,17 @@ func (diagnoser *NetworkOutboundDiagnoser) Diagnose() error {
 	if err != nil {
 		return err
 	}
-	rootPath, err := utils.CreateDiagnosticDir()
-	if err != nil {
-		return err
-	}
-
-	networkOutboundDiagnosticFile := filepath.Join(rootPath, diagnoser.GetName())
-
-	f, err := os.OpenFile(networkOutboundDiagnosticFile, os.O_CREATE|os.O_WRONLY, 0644)
-	if err != nil {
-		return fmt.Errorf("Fail to open file %s: %+v", networkOutboundDiagnosticFile, err)
-	}
-	defer f.Close()
 
 	outboundDiagnosticData := []networkOutboundDiagnosticDatum{}
 
-	for _, file := range diagnoser.networkOutboundCollector.GetCollectorFiles() {
-		t, err := os.Open(file)
-		if err != nil {
-			return fmt.Errorf("Fail to open file %s: %+v", file, err)
-		}
-		defer t.Close()
-
+	for _, data := range diagnoser.networkOutboundCollector.GetData() {
 		dataPoint := networkOutboundDiagnosticDatum{HostName: hostName}
-		scanner := bufio.NewScanner(t)
-		for scanner.Scan() {
+		lines := strings.Split(data, "\n")
+		for _, line := range lines {
 			var outboundDatum collector.NetworkOutboundDatum
-			err := json.Unmarshal([]byte(scanner.Text()), &outboundDatum)
+			err := json.Unmarshal([]byte(line), &outboundDatum)
 			if err != nil {
-				log.Printf("Unmarshal failed: %+v", err)
+				log.Printf("Unmarshal failed: %v", err)
 				continue
 			}
 
@@ -86,7 +64,7 @@ func (diagnoser *NetworkOutboundDiagnoser) Diagnose() error {
 					outboundDiagnosticData = append(outboundDiagnosticData, dataPoint)
 					setDataPoint(&outboundDatum, &dataPoint)
 				} else {
-					if int(outboundDatum.TimeStamp.Sub(dataPoint.End).Seconds()) > diagnoser.networkOutboundCollector.GetCollectIntervalInSeconds() {
+					if int(outboundDatum.TimeStamp.Sub(dataPoint.End).Seconds()) > 5 {
 						outboundDiagnosticData = append(outboundDiagnosticData, dataPoint)
 						setDataPoint(&outboundDatum, &dataPoint)
 					} else {
@@ -103,22 +81,21 @@ func (diagnoser *NetworkOutboundDiagnoser) Diagnose() error {
 
 	dataBytes, err := json.Marshal(outboundDiagnosticData)
 	if err != nil {
-		return fmt.Errorf("Fail to marshal data: %+v", err)
+		return fmt.Errorf("marshal data from NetworkOutbound Diagnoser: %w", err)
 	}
 
-	_, err = f.WriteString(string(dataBytes) + "\n")
-	if err != nil {
-		return fmt.Errorf("Fail to write data to file: %+v", err)
-	}
+	diagnoser.data["networkoutbound"] = string(dataBytes)
 
-	diagnoser.AddToDiagnoserFiles(networkOutboundDiagnosticFile)
-
-	err = utils.WriteToCRD(networkOutboundDiagnosticFile, diagnoser.GetName())
+	err = utils.WriteToCRD(string(dataBytes), diagnoser.GetName())
 	if err != nil {
-		return fmt.Errorf("Fail to write file %s to CRD: %+v", networkOutboundDiagnosticFile, err)
+		return fmt.Errorf("write data from NetworkOutbound Diagnoser to CRD: %w", err)
 	}
 
 	return nil
+}
+
+func (collector *NetworkOutboundDiagnoser) GetData() map[string]string {
+	return collector.data
 }
 
 func setDataPoint(outboundDatum *collector.NetworkOutboundDatum, dataPoint *networkOutboundDiagnosticDatum) {

pkg/utils/helper.go
@@ -58,8 +58,21 @@ func CopyFileFromHost(source, destination string) error {
 	if err != nil {
 		return fmt.Errorf("unable to retrieve source content: %w", err)
 	}
-	if err = WriteToFile(destination, sourceFile); err != nil {
-		return fmt.Errorf("unable to write source file to destination: %w", err)
+
+	if err := os.MkdirAll(filepath.Dir(destination), os.ModePerm); err != nil {
+		return fmt.Errorf("create path directories for file %s: %w", destination, err)
+	}
+
+	f, err := os.Create(destination)
+	if err != nil {
+		return fmt.Errorf("create file %s: %w", destination, err)
+	}
+
+	defer f.Close()
+
+	_, err = f.Write([]byte(sourceFile))
+	if err != nil {
+		return fmt.Errorf("write data to file %s: %w", destination, err)
 	}
 	return nil
 }
@@ -141,7 +154,7 @@ func RunCommandOnContainerWithOutputStreams(command string, arg ...string) (Comm
 	outputStreams := CommandOutputStreams{stdout.String(), stderr.String()}
 
 	if err != nil {
-		return outputStreams, fmt.Errorf("Fail to run command in container: %s", fmt.Sprint(err)+": "+stderr.String())
+		return outputStreams, fmt.Errorf("run command in container: %w", err)
 	}
 
 	return outputStreams, nil
@@ -195,67 +208,6 @@ func GetUrlWithRetries(url string, maxRetries int) ([]byte, error) {
 			return ioutil.ReadAll(resp.Body)
 		}
 	}
-}
-
-// WriteToFile writes data to a file
-func WriteToFile(fileName string, data string) error {
-	if err := os.MkdirAll(filepath.Dir(fileName), os.ModePerm); err != nil {
-		return fmt.Errorf("Fail to create path directories for file %s: %w", fileName, err)
-	}
-	f, err := os.Create(fileName)
-	if err != nil {
-		return fmt.Errorf("Fail to create file %s: %+v", fileName, err)
-	}
-	defer f.Close()
-
-	_, err = f.Write([]byte(data))
-	if err != nil {
-		return fmt.Errorf("Fail to write data to file %s: %+v", fileName, err)
-	}
-
-	return nil
-}
-
-// CreateCollectorDir creates a working dir for a collector
-func CreateCollectorDir(name string) (string, error) {
-	hostName, err := GetHostName()
-	if err != nil {
-		return "", err
-	}
-
-	creationTimeStamp, err := GetCreationTimeStamp()
-	if err != nil {
-		return "", err
-	}
-
-	rootPath := filepath.Join("/aks-periscope", strings.Replace(creationTimeStamp, ":", "-", -1), hostName, "collector", name)
-	err = os.MkdirAll(rootPath, os.ModePerm)
-	if err != nil {
-		return "", fmt.Errorf("Fail to create dir %s: %+v", rootPath, err)
-	}
-
-	return rootPath, nil
-}
-
-// CreateDiagnosticDir creates a working dir for diagnostic
-func CreateDiagnosticDir() (string, error) {
-	hostName, err := GetHostName()
-	if err != nil {
-		return "", err
-	}
-
-	creationTimeStamp, err := GetCreationTimeStamp()
-	if err != nil {
-		return "", err
-	}
-
-	rootPath := filepath.Join("/aks-periscope", strings.Replace(creationTimeStamp, ":", "-", -1), hostName, "diagnoser")
-	err = os.MkdirAll(rootPath, os.ModePerm)
-	if err != nil {
-		return "", fmt.Errorf("Fail to create dir %s: %+v", rootPath, err)
-	}
-
-	return rootPath, nil
 }
 
 // CreateKubeConfigFromServiceAccount creates kubeconfig based on creds in service account

deployment/aks-periscope.yaml
@@ -92,9 +92,6 @@ spec:
             name: collectors-config
         - secretRef:
             name: azureblob-secret
-        volumeMounts:
-        - mountPath: /aks-periscope
-          name: aks-periscope-storage
         resources:
           requests:
             memory: "500Mi"
@@ -102,11 +99,6 @@ spec:
           limits:
             memory: "2000Mi"
             cpu: "1000m"
-      volumes:
-      - name: aks-periscope-storage
-        hostPath:
-          path: /var/log/aks-periscope
-          type: DirectoryOrCreate
 ---
 apiVersion: v1
 kind: Secret

go.mod
@@ -4,8 +4,8 @@ go 1.15
 
 require (
 	github.com/Azure/azure-storage-blob-go v0.7.0
-	github.com/Azure/go-autorest/autorest/adal v0.9.13 // indirect
-	github.com/golangci/golangci-lint v1.40.1 // indirect
+	github.com/Azure/go-autorest/autorest/adal v0.9.14 // indirect
 	github.com/kr/pretty v0.2.1 // indirect
 	github.com/onsi/gomega v1.13.0
+	github.com/pkg/errors v0.9.1 // indirect
 )

pkg/collector/collector.go
@@ -1,103 +1,0 @@
-package collector
-
-import (
-	"path/filepath"
-
-	"github.com/Azure/aks-periscope/pkg/interfaces"
-	"github.com/Azure/aks-periscope/pkg/utils"
-)
-
-// Type defines Collector Type
-type Type int
-
-const (
-	// DNS defines DNS Collector Type
-	DNS Type = iota
-	// ContainerLogs defines ContainerLogs Collector Type
-	ContainerLogs
-	//Helm defines Helm Collector Type
-	Helm
-	// IPTables defines IPTables Collector Type
-	IPTables
-	// KubeletCmd defines KubeletCmd Collector Type
-	KubeletCmd
-	// KubeObjects defines KubeObjects Collector Type
-	KubeObjects
-	// NetworkOutbound defines NetworkOutbound Collector Type
-	NetworkOutbound
-	// NodeLogs defines NodeLogs Collector Type
-	NodeLogs
-	// Osm defines Open Service Mesh Collector Type
-	Osm
-	// SystemLogs defines SystemLogs Collector Type
-	SystemLogs
-	// SystemPerf defines SystemPerf Collector Type
-	SystemPerf
-)
-
-// Name returns type name
-func (t Type) name() string {
-	return [...]string{"dns", "containerlogs", "helm", "iptables", "kubeletcmd", "kubeobjects", "networkoutbound", "nodelogs", "osm", "systemlogs", "systemperf"}[t]
-}
-
-// BaseCollector defines Base Collector
-type BaseCollector struct {
-	collectorType            Type
-	collectIntervalInSeconds int
-	collectorFiles           []string
-	exporter                 interfaces.Exporter
-}
-
-// GetName gets collector name
-func (b *BaseCollector) GetName() string {
-	return b.collectorType.name()
-}
-
-// GetCollectIntervalInSeconds gets collector interval in seconds
-func (b *BaseCollector) GetCollectIntervalInSeconds() int {
-	return b.collectIntervalInSeconds
-}
-
-// GetCollectorFiles gets collector files
-func (b *BaseCollector) GetCollectorFiles() []string {
-	return b.collectorFiles
-}
-
-// AddToCollectorFiles adds a file to collector files
-func (b *BaseCollector) AddToCollectorFiles(file string) {
-	b.collectorFiles = append(b.collectorFiles, file)
-}
-
-// Export implements the interface method
-func (b *BaseCollector) Export() error {
-	if b.exporter != nil {
-		return b.exporter.Export(b.collectorFiles)
-	}
-
-	return nil
-}
-
-// CollectKubectlOutputToCollectorFiles collects output of a given kubectl command to a file.
-// Returns kubectl's stderr output if stdout output is empty.
-func (b *BaseCollector) CollectKubectlOutputToCollectorFiles(rootPath string, fileName string, kubeCmds []string) error {
-	outputStreams, err := utils.RunCommandOnContainerWithOutputStreams("kubectl", kubeCmds...)
-	if err != nil {
-		return err
-	}
-
-	// If kubectl stdout output is empty, i.e., there is no resource of this type within the cluster
-	// the absence of this resource is logged in the file with the relevant message from stderr (Ex: "No resource found...").
-	output := outputStreams.Stdout
-	if len(output) == 0 {
-		output = outputStreams.Stderr
-	}
-
-	resourceFile := filepath.Join(rootPath, fileName)
-	if err = utils.WriteToFile(resourceFile, output); err != nil {
-		return err
-	}
-
-	b.AddToCollectorFiles(resourceFile)
-
-	return nil
-}

commit ba215e55e606bfa7df5411da74da34641547b403

Fix for diagnosers to CRD, broken since cce80006

cmd/aks-periscope/aks-periscope.go
@@ -121,8 +121,8 @@ func initializeComponents(creationTimeStamp string, hostname string) ([]interfac
 
 	//diagnosers
 	//NOTE currently the collector instance objects are shared between the collector itself and things which use it as a dependency
-	networkConfigDiagnoser := diagnoser.NewNetworkConfigDiagnoser(dnsCollector, kubeletCmdCollector)
-	networkOutboundDiagnoser := diagnoser.NewNetworkOutboundDiagnoser(networkOutboundCollector)
+	networkConfigDiagnoser := diagnoser.NewNetworkConfigDiagnoser(hostname, dnsCollector, kubeletCmdCollector)
+	networkOutboundDiagnoser := diagnoser.NewNetworkOutboundDiagnoser(hostname, networkOutboundCollector)
 	selectedDiagnosers := selectDiagnosers(
 		map[string]interfaces.Diagnoser{
 			networkConfigDiagnoser.GetName():   networkConfigDiagnoser,

pkg/diagnoser/networkconfig_diagnoser.go
@@ -20,16 +20,18 @@ type networkConfigDiagnosticDatum struct {
 
 // NetworkConfigDiagnoser defines a NetworkConfig Diagnoser struct
 type NetworkConfigDiagnoser struct {
+	hostName string
 	dnsCollector        *collector.DNSCollector
 	kubeletCmdCollector *collector.KubeletCmdCollector
 	data                map[string]string
 }
 
 // NewNetworkConfigDiagnoser is a constructor
-func NewNetworkConfigDiagnoser(dnsCollector *collector.DNSCollector, kubeletCmdCollector *collector.KubeletCmdCollector) *NetworkConfigDiagnoser {
+func NewNetworkConfigDiagnoser(hostName string, dnsCollector *collector.DNSCollector, kubeletCmdCollector *collector.KubeletCmdCollector) *NetworkConfigDiagnoser {
 	return &NetworkConfigDiagnoser{
 		dnsCollector:        dnsCollector,
 		kubeletCmdCollector: kubeletCmdCollector,
+		hostName: 		     hostName,
 		data:                make(map[string]string),
 	}
 }
@@ -91,7 +93,7 @@ func (diagnoser *NetworkConfigDiagnoser) Diagnose() error {
 
 	diagnoser.data["networkconfig"] = string(dataBytes)
 
-	if err = utils.WriteToCRD(string(dataBytes), diagnoser.GetName()); err != nil {
+	if err = utils.WriteToCRD(dataBytes, diagnoser.GetName(), hostName); err != nil {
 		return fmt.Errorf("write data from NetworkConfig Diagnoser to CRD: %w", err)
 	}
 

pkg/diagnoser/networkoutbound_diagnoser.go
@@ -21,13 +21,15 @@ type networkOutboundDiagnosticDatum struct {
 
 // NetworkOutboundDiagnoser defines a NetworkOutbound Diagnoser struct
 type NetworkOutboundDiagnoser struct {
+	hostName 				 string
 	networkOutboundCollector *collector.NetworkOutboundCollector
 	data                     map[string]string
 }
 
 // NewNetworkOutboundDiagnoser is a constructor
-func NewNetworkOutboundDiagnoser(networkOutboundCollector *collector.NetworkOutboundCollector) *NetworkOutboundDiagnoser {
+func NewNetworkOutboundDiagnoser(hostName string, networkOutboundCollector *collector.NetworkOutboundCollector) *NetworkOutboundDiagnoser {
 	return &NetworkOutboundDiagnoser{
+		hostName: 		     hostName,
 		networkOutboundCollector: networkOutboundCollector,
 		data:                     make(map[string]string),
 	}
@@ -86,7 +88,7 @@ func (diagnoser *NetworkOutboundDiagnoser) Diagnose() error {
 
 	diagnoser.data["networkoutbound"] = string(dataBytes)
 
-	err = utils.WriteToCRD(string(dataBytes), diagnoser.GetName())
+	err = utils.WriteToCRD(dataBytes, diagnoser.GetName(), hostName)
 	if err != nil {
 		return fmt.Errorf("write data from NetworkOutbound Diagnoser to CRD: %w", err)
 	}

pkg/utils/helper.go
@@ -283,22 +283,12 @@ func GetCreationTimeStamp() (string, error) {
 }
 
 // WriteToCRD writes diagnostic data to CRD
-func WriteToCRD(fileName string, key string) error {
-	hostName, err := GetHostName()
-	if err != nil {
-		return err
-	}
+func WriteToCRD(jsonBytes []byte, key string, hostName string) error {
 
 	crdName := "aks-periscope-diagnostic" + "-" + hostName
-
-	jsonBytes, err := ioutil.ReadFile(fileName)
-	if err != nil {
-		return err
-	}
-
 	patchContent := fmt.Sprintf("{\"spec\":{%q:%q}}", key, string(jsonBytes))
 
-	_, err = RunCommandOnContainer("kubectl", "-n", "aks-periscope", "patch", "apd", crdName, "-p", patchContent, "--type=merge")
+	_, err := RunCommandOnContainer("kubectl", "-n", "aks-periscope", "patch", "apd", crdName, "-p", patchContent, "--type=merge")
 	if err != nil {
 		return err
 	}