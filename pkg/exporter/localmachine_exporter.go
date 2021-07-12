d92e1fbb5d6aea12818901acd51206deafdb2990
david kydd <david.kydd@microsoft.com>
2021-07-12 12:13:56 +1200

    alignment, remove unused var, move defer after errCheck

--- pkg/exporter/localmachine_exporter.go
+++ pkg/exporter/localmachine_exporter.go
@@ -9,13 +9,13 @@ import (
 )
 
 // LocalMachineExporter defines an Local Machine Exporter
-type LocalMachineExporter struct{
-	hostname     string
+type LocalMachineExporter struct {
+	hostname string
 }
 
 func NewLocalMachineExporter(hostname string) *LocalMachineExporter {
 	return &LocalMachineExporter{
-		hostname:     hostname,
+		hostname: hostname,
 	}
 }
 
@@ -24,30 +24,28 @@ func (exporter *LocalMachineExporter) GetName() string {
 }
 
 // Export implements the interface method
-func (exporter *LocalMachineExporter) Export(_ interfaces.DataProducer) error {
+func (exporter *LocalMachineExporter) Export(interfaces.DataProducer) error {
 	//TODO perhaps this should export to files on the localMachine also
 	return nil
 }
 
 //ExportReader implements the interface method
 func (exporter *LocalMachineExporter) ExportReader(name string, reader io.ReadSeeker) error {
-
 	//TODO might need to change the location this file is written
 	outFile, err := os.Create(name)
-	defer outFile.Close()
-
 	if err != nil {
 		return err
 	}
+	defer outFile.Close()
 
 	bytes, err := ioutil.ReadAll(reader)
 	if err != nil {
-		log.Printf("read from reader: %v",  err)
+		log.Printf("read from reader: %v", err)
 	}
 
 	_, err = outFile.Write(bytes)
 	if err != nil {
-		log.Printf("write to outfile: %v",  err)
+		log.Printf("write to outfile: %v", err)
 		return err
 	}
 
